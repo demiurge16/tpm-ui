54369bc00d5d014e36d8ca5bf5bb07b1f7d8428f
Oleh Shatskyi <olegshatskyj@gmail.com>
2023-01-31 19:34:24 +0100

    Fix environments

--- src/Environment.ts
+++ src/Environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
-  apiUrl: process.env.TPM_API_URL || 'http://localhost:8080/api',
-  env: process.env.TPM_ENV || 'dev',
+  apiUrl: process.env.REACT_APP_TPM_API_URL || 'http://localhost:8080/api/v1',
+  env: process.env.REACT_APP_TPM_ENV || 'dev',
 }
